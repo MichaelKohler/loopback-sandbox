fe79e7c282b2adcad753f4fcbfb7711c2ff1d8f0
Michael Kohler <mkohler@picobudget.com>
2016-04-28 17:24:22 +0200

    Add lodash + create default users at boot time

---
+++ server/boot/addUser.js
@@ -0,0 +1,25 @@
+User.create([
+    {username: 'John', email: 'john@doe.com', password: 'opensesame'},
+    {username: 'Jane', email: 'jane@doe.com', password: 'opensesame'},
+    {username: 'Bob', email: 'bob@projects.com', password: 'opensesame'}
+], function(err, users) {
+    if (err) return debug('%j', err);
+
+    // Create the admin role
+    Role.create({
+      name: 'admin'
+    }, function(err, role) {
+      if (err) return debug(err);
+      debug(role);
+
+      // Make Bob an admin
+      role.principals.create({
+        principalType: RoleMapping.USER,
+        principalId: users[2].id
+      }, function(err, principal) {
+        if (err) return debug(err);
+        debug(principal);
+      });
+    });
+  });
+};
